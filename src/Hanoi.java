dcca4844bdf828c964840fecd814af4dac24f73c
considinej yang <considinejyang@gmail.com>
2022-12-03 15:23:33 +0800

    Add hanoi

---
+++ src/Hanoi.java
@@ -0,0 +1,137 @@
+import java.util.Stack;
+
+public class Hanoi {
+    public static void hanoi1(int n) {
+        if(n>0) {
+            func(n, "left", "right", "mid");
+        }
+    }
+    public static void func(int N, String from, String to, String other) {
+        if (N == 1) {
+            System.out.println("Move 1 from " + from + " to " + to);
+        } else {
+            func(N-1, from, other, to);
+            System.out.println("Move " + N + " from " + " to " + to);
+            func(N -1, other, to, from);
+        }
+    }
+
+    public static void hanoi2(int n) {
+        leftToRight(n);
+    }
+
+    //把1~N层圆盘，从左到右
+    public static void leftToRight(int n) {
+        if (n == 1) {
+            System.out.println("Move 1 from left to right");
+            return;
+        }
+        leftToMid(n - 1);
+        System.out.println("Move " + n + " from left to right");
+        midToRight(n - 1);
+    }
+    
+    //把1～N层圆盘 从左到中
+    public static void leftToMid(int n) {
+        if (n == 1) {
+            System.out.println("Move 1 from left to mid");
+            return;
+        }
+        leftToRight(n - 1);
+        System.out.println("Move " + n + " from left to mid");
+        rightToMid(n - 1);
+    }
+
+    public static void rightToMid(int n) {
+        if (n == 1) {
+            System.out.println("Move 1 from right to mid");
+            return;
+        }
+        rightToLeft(n - 1);
+        System.out.println("Move " + n + " from right to mid");
+        leftToMid(n - 1);
+    }
+
+    public static void midToRight(int n) {
+        if (n == 1) {
+            System.out.println("Move 1 from mid to right");
+            return;
+        }
+        midToLeft(n - 1);
+        System.out.println("Move " + n + " from mid to right");
+        leftToRight(n - 1);
+    }
+
+    public static void midToLeft(int n) {
+        if (n == 1) {
+            System.out.println("Move 1 from mid to left");
+            return;
+        }
+        midToRight(n - 1);
+        System.out.println("Move " + n + " from mid to left");
+        rightToLeft(n - 1);
+    }
+
+    public static void rightToLeft(int n) {
+        if (n == 1) {
+            System.out.println("Move 1 from right to left");
+            return;
+        }
+        rightToMid(n - 1);
+        System.out.println("Move " + n + " from right to left");
+        midToLeft(n - 1);
+    }
+
+
+    public static class Record {
+        public boolean finish;
+        public int base;
+        public String from;
+        public String to;
+        public String other;
+
+        public Record(boolean finish, int base, String from, String to, String other) {
+            this.finish = finish;
+            this.base = base;
+            this.from = from;
+            this.to = to;
+            this.other = other;
+        }
+    }
+
+    public static void hanoi3(int N) {
+        if (N < 1) {
+            return;
+        }
+        Stack<Record> stack = new Stack<>();
+        stack.add(new Record(false, N, "left", "right", "mid"));
+        while (!stack.isEmpty()) {
+            Record cur = stack.pop();
+            if (cur.base == 1) {
+                System.out.println("Move 1 from " + cur.from + " to " + cur.to);
+                if (!stack.isEmpty()) {
+                    stack.peek().finish = true;
+                }
+            } else {
+                if (!cur.finish) {
+                    stack.push(cur);
+                    stack.push(new Record(false,  cur.base - 1, cur.from, cur.other, cur.to));
+                } else {
+                    System.out.println("Move " + cur.base + " from " + cur.from + " to " + cur.to);
+                    stack.push(new Record(false, cur.base - 1, cur.other, cur.to, cur.from));
+                }
+            }
+        }
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        int n =3;
+        hanoi1(3);
+        System.out.println("==========================");
+        hanoi2(3);
+    }
+}
